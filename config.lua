d0effed60b9c678862fd0d561b546a8bd34d9fbb
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-03-22 17:58:38 -0600

    OG Settings Save

--- config.lua
+++ config.lua
@@ -1,45 +1,78 @@
 Config = Config or {}
 
-Config.BusinessName = "Vehicle Sales Contract - Larry's Vehicle Sales"
-Config.SellVehicle = vector3(1235.43, 2730.95, 37.91)
-Config.SellVehicleBack = vector3(1235.1, 2740.7, 37.68)
-Config.BuyVehicle = vector4(1213.31, 2735.4, 38.27, 182.5)
+Config.BusinessName = "Badget Car Dealer"
+Config.SellVehicle = vector3(-239.31, -1183.8, 22.71)
+Config.SellVehicleBack = vector3(-245.57, -1168.07, 22.71)
+Config.BuyVehicle = vector4(-221.3, -1155.54, 23.02, 350.69)
 
 Config.OccasionSlots = {
-    { loc = vector3(1237.07, 2699, 38.27), h = 1.5 },
-    { loc = vector3(1232.98, 2698.92, 38.27), h = 2.5 },
-    { loc = vector3(1228.9, 2698.78, 38.27), h = 3.5 },
-    { loc = vector3(1224.9, 2698.51, 38.27), h = 2.5 },
-    { loc = vector3(1220.93, 2698.28, 38.27), h = 2.5 },
-    { loc = vector3(1216.97, 2698.05, 38.27), h = 0.5 },
-    { loc = vector3(1216.67, 2709.21, 38.27), h = 1.5 },
-    { loc = vector3(1220.67, 2709.26, 38.27), h = 1.5 },
-    { loc = vector3(1224.53, 2709.27, 38.27), h = 2.5 },
-    { loc = vector3(1228.52, 2709.42, 38.27), h = 1.5 },
-    { loc = vector3(1232.53, 2709.49, 38.27), h = 1.5 },
-    { loc = vector3(1236.71, 2709.51, 38.27), h = 1.5 },
-    { loc = vector3(1216.41, 2717.99, 38.27), h = 1.5 },
-    { loc = vector3(1220.39, 2718, 38.27), h = 0.5 },
-    { loc = vector3(1224.35, 2718.07, 38.27), h = 1.5 },
-    { loc = vector3(1228.41, 2718.22, 38.27), h = 1.5 },
-    { loc = vector3(1249.63, 2707.84, 38.27), h = 99.5 },
-    { loc = vector3(1248.92, 2712.25, 38.27), h = 101.5 },
-    { loc = vector3(1247.3, 2716.59, 38.27), h = 120.5 },
-    { loc = vector3(1244.09, 2720.4, 38.27), h = 149.5 },
-    { loc = vector3(1239.93, 2722.39, 38.27), h = 163.5 },
-    { loc = vector3(1248.28, 2727.41, 38.53), h = 338.5 },
-    { loc = vector3(1251.84, 2725.65, 38.52), h = 331.5 },
-    { loc = vector3(1255.19, 2723.21, 38.44), h = 309.5 },
-    { loc = vector3(1257.28, 2719.77, 38.49), h = 296.5 },
-    { loc = vector3(1258.43, 2716.2, 38.46), h = 285.5 },
-    { loc = vector3(1258.92, 2712.28, 38.37), h = 270.5 },
-    { loc = vector3(1258.9, 2708.3, 38.21), h = 269.5 },
-    { loc = vector3(1258.87, 2704.21, 38.05), h = 271.5 },
-    { loc = vector3(1269.11, 2694.67, 37.85), h = 184.5 },
-    { loc = vector3(1273.53, 2694.98, 37.87), h = 184.5 },
-    { loc = vector3(1277.56, 2695.43, 37.87), h = 183.5 },
-    { loc = vector3(1281.53, 2695.67, 37.88), h = 186.5 },
-    { loc = vector3(1285.51, 2696.02, 37.88), h = 185.5 },
-    { loc = vector3(1289.74, 2696.27, 37.88), h = 182.5 },
-    { loc = vector3(1293.75, 2696.47, 37.86), h = 184.5 }
+    { loc = vector3(-152.9, -1170.01, 23.15), h = 270.71 }, -- Brioso2
+    { loc = vector3(-152.99, -1166.55, 23.08), h = 269.12 }, -- Club
+    { loc = vector3(-147.44, -1161.73, 23.21), h = 180.28 }, -- Kanjo
+    { loc = vector3(-144.01, -1161.68, 23.59), h = 181.26 }, -- Blista
+    { loc = vector3(-139.19, -1162.07, 23.37), h = 181.33 }, -- Stanier
+    { loc = vector3(-136.14, -1161.99, 23.3), h = 181.62 }, -- Primo
+    { loc = vector3(-132.56, -1166.55, 23.27), h = 89.39 }, -- Manana
+    { loc = vector3(-132.6, -1170.15, 23.43), h = 91.62 }, -- Picador
+    { loc = vector3(-132.42, -1175.18, 23.44), h = 91.74 }, -- Vigero
+    { loc = vector3(-132.38, -1178.59, 23.24), h = 90.12 }, -- Tampa
+    { loc = vector3(-132.31, -1182.27, 23.21), h = 90.77 }, -- Virgo
+    { loc = vector3(-136.61, -1183.09, 22.96), h = 0.18 }, -- Warrener
+    { loc = vector3(-139.53, -1183.31, 23.47), h = 1.42 }, -- Glendale
+    { loc = vector3(-143.54, -1183.62, 23.17), h = 0.07 }, -- Stratum
+    { loc = vector3(-146.53, -1184.42, 23.44), h = 356.84 }, -- Ingot
+    { loc = vector3(-149.49, -1183.17, 23.31), h = 358.79 }, -- Enus
+    { loc = vector3(-146.79, -1176.41, 23.28), h = 359.03 }, -- Premier
+    { loc = vector3(-144.16, -1176.14, 22.98), h = 357.67 }, -- Warrener2
+    { loc = vector3(-141.24, -1176.19, 23.47), h = 0.61 }, -- Fugitive
+    { loc = vector3(-138.25, -1176.53, 23.48), h = 357.49 }, -- Surge
+    { loc = vector3(-146.75, -1169.91, 23.46), h = 1.63 }, -- Regina
+    { loc = vector3(-143.89, -1169.59, 23.28), h = 2.3 }, -- Premier
+    { loc = vector3(-141.09, -1169.57, 23.34), h = 357.49 }, -- Asea
+    { loc = vector3(-138.08, -1170.14, 23.08), h = 357.12 }, -- Club
+	{ loc = vector3(-159.51, -1182.6, 22.71), h = 0.4 }, -- Picador
+	{ loc = vector3(-162.32, -1182.8, 22.52), h = 1.11 }, -- Futo
+    { loc = vector3(-166.67, -1182.75, 22.75), h = 0.21 }, -- Glendale
+    { loc = vector3(-170.87, -1182.6, 22.69), h = 1.39 }, -- Tailgater
+    { loc = vector3(-173.84, -1182.74, 22.56), h = 359.0 }, -- Asterope
+    { loc = vector3(-176.89, -1182.62, 22.65), h = 1.67 }, -- Stanier
+	{ loc = vector3(-180.04, -1182.72, 22.64), h = 3.47 }, -- Schafter
+	{ loc = vector3(-185.63, -1183.33, 22.26), h = 1.21 }, -- Warrener2
+	{ loc = vector3(-188.31, -1183.11, 22.36), h = 0.36 }, -- Club
+    { loc = vector3(-191.09, -1182.87, 22.72), h = 0.25 }, -- Issi2
+    { loc = vector3(-193.75, -1183.26, 22.54), h = 0.64 }, -- Dilettante
+    { loc = vector3(-196.59, -1183.33, 22.61), h = 2.44 }, -- Prairie
+    { loc = vector3(-201.76, -1182.94, 22.43), h = 1.99 }, -- Tulip
+	{ loc = vector3(-204.91, -1182.86, 22.55), h = 0.28 }, -- Chino
+	{ loc = vector3(-207.95, -1182.5, 22.52), h = 358.22 }, -- Sabregt
+    { loc = vector3(-158.59, -1172.9, 22.57), h = 180.23 }, -- Kalahari
+    { loc = vector3(-158.24, -1166.56, 22.57), h = 177.07 }, -- Bifta
+    { loc = vector3(-166.56, -1166.21, 22.84), h = 182.29 }, -- Mesa3
+	{ loc = vector3(-167.03, -1173.89, 22.64), h = 177.82 }, -- Intruder
+	{ loc = vector3(-172.23, -1174.17, 22.55), h = 181.39 }, -- Emperor
+    { loc = vector3(-173.32, -1165.99, 22.54), h = 178.65 }, -- Primo
+    { loc = vector3(-177.99, -1165.74, 22.64), h = 178.72 }, -- Intruder
+    { loc = vector3(-175.41, -1173.97, 22.64), h = 179.63 }, -- Schafter
+    { loc = vector3(-178.95, -1174.02, 22.65), h = 176.92 }, -- Stanier
+	{ loc = vector3(-183.76, -1174.43, 22.75), h = 197.58 }, -- Fugitive
+	{ loc = vector3(-187.66, -1173.81, 22.56), h = 200.37 }, -- Asterope
+	{ loc = vector3(-191.04, -1174.09, 22.24), h = 203.66 }, -- Warrener
+    { loc = vector3(-194.67, -1174.16, 22.58), h = 196.59 }, -- Primo
+    { loc = vector3(-203.43, -1173.26, 22.75), h = 180.86 }, -- Glendale
+    { loc = vector3(-206.21, -1173.35, 22.57), h = 180.5 }, -- Washington
+    { loc = vector3(-208.99, -1173.45, 22.44), h = 182.27 }, -- Yosemite Rancher
+	{ loc = vector3(-212.3, -1173.43, 22.8), h = 180.93 }, -- Sadler
+	{ loc = vector3(-215.43, -1173.56, 22.55), h = 181.46 }, -- Stafford
+    { loc = vector3(-203.38, -1164.99, 22.41), h = 182.61 }, -- Tailgator
+    { loc = vector3(-206.12, -1165.51, 22.71), h = 179.9 }, -- Ingot
+    { loc = vector3(-209.2, -1165.82, 22.67), h = 179.56 }, -- Cognoscenti
+	{ loc = vector3(-212.16, -1165.23, 22.45), h = 181.47 }, -- Stratum
+	{ loc = vector3(-215.15, -1165.57, 22.56), h = 178.89 }, -- Cognoscenti
+    { loc = vector3(-223.74, -1165.39, 22.61), h = 182.07 }, -- Assea
+    { loc = vector3(-223.69, -1171.36, 22.64), h = 182.04 }, -- Glendale
+    { loc = vector3(-221.14, -1184.5, 23.06), h = 269.96 }, -- Everon
+    { loc = vector3(-229.47, -1171.13, 22.69), h = 91.7 }, -- Tailgator
+	{ loc = vector3(-229.41, -1168.07, 22.63), h = 88.14 }, -- Gauntlet
+	{ loc = vector3(-229.27, -1164.56, 22.68), h = 88.71 }, -- Rancher
+    	
 }
