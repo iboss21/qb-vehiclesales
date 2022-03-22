1765440242a9b64f7dd7acb22a5e5a3e7c588f45
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-03-22 17:58:01 -0600

    Framework Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,11 +7,16 @@ version '1.0.0'
 ui_page 'html/ui.html'
 
 shared_scripts {
-	'config.lua'
+	'config.lua',
+	'@qb-core/shared/locale.lua',
+	'locales/en.lua'
 }
 
 client_script 'client/main.lua'
-server_script 'server/main.lua'
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
 
 files {
 	'html/reset.css',
